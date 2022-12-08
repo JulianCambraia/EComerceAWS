ad696630b825940a13e4ef67c75b50af643c331d
JulianCambraia <juliancambraia@gmail.com>
2022-12-08 11:50:26 -0300

    Add boostrap CDK Aplication

--- lib/e_comerce_aws-stack.ts
+++ lib/e_comerce_aws-stack.ts
@@ -5,7 +5,6 @@ import { Construct } from 'constructs';
 export class EComerceAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     // The code that defines your stack goes here
 
     // example resource
